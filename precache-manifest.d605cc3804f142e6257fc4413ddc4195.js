a04cac65943b4b055d108bb69a69af9b1f1aaa9a
shultztom <shultzthomask@sau.edu>
2018-11-08 18:53:16 -0600

    Updates

---
+++ precache-manifest.d605cc3804f142e6257fc4413ddc4195.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "723d02c393abe589119f",
+    "url": "/dog-generator-react/static/js/runtime~main.723d02c3.js"
+  },
+  {
+    "revision": "7710bb9c74eb7f0b5849",
+    "url": "/dog-generator-react/static/js/main.7710bb9c.chunk.js"
+  },
+  {
+    "revision": "b8339f132f1d141e03d1",
+    "url": "/dog-generator-react/static/js/1.b8339f13.chunk.js"
+  },
+  {
+    "revision": "7710bb9c74eb7f0b5849",
+    "url": "/dog-generator-react/static/css/main.f5ffcd55.chunk.css"
+  },
+  {
+    "revision": "49b3f584d5ea53207bc783509a32be7d",
+    "url": "/dog-generator-react/index.html"
+  }
+];
